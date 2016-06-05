afbb2eccb689f6165caba493cdcf9146ee071db7
cjwebb <colinjameswebb@hotmail.com>
2016-06-05 14:12:12 +0100

    Configure Dokku

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :munchcal, Munchcal.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "munchcal.com", port: 80],
+  url: [host: System.get_env("HOSTNAME"), port: 80],
   root: ".",
   cache_static_manifest: "priv/static/manifest.json",
   server: true
